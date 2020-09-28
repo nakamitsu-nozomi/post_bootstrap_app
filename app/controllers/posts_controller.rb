4e72bb48926cbd860a04d5b4f944e63344d4df27
nozomi <kkk@docomo.ne.jp>
2020-09-28 15:57:34 +0900

    各メッセージ  に対し，メッセージを投稿したユーザー をDBに問い合わせていることを解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
  before_action :set_post, only: %i[show edit update destroy]
  before_action :move_to_index,except: :index
   def index
-    @posts=Post.order(id: :asc)
+    @posts=Post.order(id: :asc).includes(:user)
   end
 
   def show
