7c3d0353fed315b0e8391e1a8fc443622e5b56d3
nozomi <kkk@docomo.ne.jp>
2020-09-29 18:10:20 +0900

    Post,User,Likeモデルの関連付け、バリテーションを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: {maximum: 50}
   validates :content, presence: true, length: {maximum: 500}
   belongs_to :user
+  has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_090734) do
+ActiveRecord::Schema.define(version: 2020_09_29_085659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -37,4 +47,6 @@ ActiveRecord::Schema.define(version: 2020_09_27_090734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
 end
