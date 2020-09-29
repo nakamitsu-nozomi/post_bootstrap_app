fbc608212650a7d6f9fdff740fc5a6e235c51fd8
nozomi <kkk@docomo.ne.jp>
2020-09-29 18:19:48 +0900

    中間テーブルを経由した 多対多 の関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: {maximum: 500}
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   mount_uploader :image, ImageUploader
 end
