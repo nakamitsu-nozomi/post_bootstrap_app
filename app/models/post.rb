5a6764c7c6fac7060b6a8ea51060aa51fac37c5b
nozomi <kkk@docomo.ne.jp>
2020-09-30 14:48:16 +0900

    unknon_key error 解決

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   mount_uploader :image, ImageUploader
+
+   # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」
+   def liked_by?(user)
+    likes.find_by(user_id: user.id).present?
+   end
 end
