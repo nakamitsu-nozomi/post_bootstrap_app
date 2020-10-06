a5508ae677d66844b480684dd5d5c3d02ab6c3d5
nozomi <kkk@docomo.ne.jp>
2020-10-06 09:15:58 +0900

    userモデルにフォロー機能のメソッドを書く

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
 
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 
 end
