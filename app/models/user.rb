a613a4dbcfb84d50bdb7622671782e055f4bd788
nozomi <kkk@docomo.ne.jp>
2020-10-06 09:09:00 +0900

    relationshipsのマイグレーションファイルを編集&実行
    relationshipsモデルとuserモデルにアソシエーションを書く

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   has_many :posts,dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "follow_id"
+  has_many :followers,through: :reverse_of_relationships,source: :user
+
      
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
 end
