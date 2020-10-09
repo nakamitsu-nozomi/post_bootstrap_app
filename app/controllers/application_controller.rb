3de147a89227e038c09589c317445370c7d6b8da
nozomi <kkk@docomo.ne.jp>
2020-10-09 10:09:59 +0900

    mypageトップ画面見た目整えた。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickrname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_icon])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:user_icon])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_icon])
   end
 
 
